d0e9cac823c2436b54057e9bdc59f36f2ee2f13a
Jason <ja2415n@gmail.com>
2021-11-14 00:25:00 -0800

    Add comments

--- src/lib/TextContainer.svelte
+++ src/lib/TextContainer.svelte
@@ -1,14 +1,16 @@
 <script>
-  import { fade, blur } from 'svelte/transition';
+  import { fade } from 'svelte/transition';
   export let string;
 </script>
 
-<div class="fixed bottom-[2vh] flex justify-center w-screen" on:click={foo}>
+<div class="fixed bottom-[2vh] flex justify-center w-screen">
   <p
     class="text-xl md:text-xl lg:text-3xl text-white text-center overflow-ellipsis min-w-[50ch]
             max-w-[80ch] p-3 rounded-2xl bg-black after:filler"
   >
-    {#each string.slice(Math.max(0, string.length - 80)) as char}
+    <!-- Display the entire string if there are less than 75 characters, otherwise display the-->
+    <!-- last 75 characters of the string.-->
+    {#each string.slice(Math.max(0, string.length - 75)) as char}
       <span in:fade={{ duration: 350 }}>{char}</span>
     {/each}
   </p>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,6 +2,7 @@
   import '../app.css';
   import TextContainer from '$lib/TextContainer.svelte';
 
+  // Holds all the valid character inputs.
   const keys = new Set('abcdefghijklmnopqrstuvwxyz`1234567890-=~!@#$%^&*()_+[]\\;\',./{}|:"<>?');
   let string = '';
 
@@ -9,14 +10,21 @@
     const key = event.key;
     const ctrl = event.ctrlKey;
 
+    // If the backspace is pressed, check if CTRL is also pressed. If CTRL is also pressed then
+    // then delete the entire last phrase by calling deletePhrase().
     if (key === 'Backspace') {
       string = string.slice(0, -1);
       if (ctrl) string = wordDelete();
     }
+
+    // Only allow for one trailing space at the end of the string.
     if (key === ' ') string += string.at(-1) === ' ' ? '' : key;
+
     if (keys.has(key.toLowerCase())) string += key;
   }
 
+  // Calculate the last phrase by finding the last space in the string. If there is no space
+  // found, then the entire string is cleared.
   function deletePhrase() {
     const index = string.lastIndexOf(' ');
     if (index === -1) return '';
